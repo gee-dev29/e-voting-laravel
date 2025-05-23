4c4ff987b4d59e5b34e4a1bc53501cb4ae0c04f5
gee-dev29 <maduegodwin2605@gmail.com>
2025-05-23 16:23:53 +0100

    fix

---
+++ app/Service/OTPService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http;
+
+use App\Models\User;
+
+class OtpService
+{
+  public function generateOtpForUser(User $user): string
+  {
+    $otp = rand(100000, 999999);
+
+    $user->otp = (object) [
+      'value' => $otp,
+      'createdOn' => now()->toISOString(),
+      'expiresOn' => now()->addMinutes(10)->toISOString(),
+    ];
+    $user->save();
+
+    return $otp;
+  }
+}
