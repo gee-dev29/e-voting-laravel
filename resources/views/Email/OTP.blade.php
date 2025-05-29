ddf69f3c0898cce0c965b1b96f146bbd06ed7f41
gee-dev29 <maduegodwin2605@gmail.com>
2025-05-29 17:51:34 +0100

    otp blade

---
+++ resources/views/Email/OTP.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Your OTP Code</title>
+</head>
+
+<body>
+  <h1>Hello!</h1>
+  <p>Your OTP code is:</p>
+  <h2>{{ $otp }}</h2>
+  <p>This code will expire in 10 minutes.</p>
+</body>
+
+</html>
