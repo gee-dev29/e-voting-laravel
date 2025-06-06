db83d8184a18b87261f9d0327255d6c3d4980b83
gee-dev29 <maduegodwin2605@gmail.com>
2025-06-06 15:46:13 +0100

    resolved

--- app/Http/Controllers/FindUserByEmail.php
+++ app/Http/Controllers/FindUserByEmail.php
@@ -7,25 +7,28 @@ use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Symfony\Component\HttpFoundation\Response;
 
-class FindUserByEmail
+class FindUserByEmail extends Controller
 {
-  public function __invoke(Request $request, Closure $next): Response
-  {
-    $validator = Validator::make($request->all(), [
-      'email' => 'required|string|email'
-    ]);
+    public function __invoke(Request $request, Closure $next)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|string|email'
+            ]);
 
-    $validatedData = $validator->validate();
-    $user = User::where('email', $validatedData['email'])->first();
+            $validatedData = $validator->validate();
+            $user = User::where('email', $validatedData['email'])->first();
 
-    $validatedData = $validator->validate();
-    if ($validatedData['email'] !== $user->email()) {
-      throw UserException::InvalidLoginCredential($user);
+            $validatedData = $validator->validate();
+            if ($validatedData['email'] !== $user->email()) {
+                throw UserException::InvalidLoginCredential($user);
+            }
+
+            $request->attributes->set('user', $user);
+            return $next($request);
+        } catch (\Throwable $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
     }
-
-    $request->attributes->set('user', $user);
-    return $next($request);
-  }
 }
