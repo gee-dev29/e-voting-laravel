3e3b8b9c8306f6c6e808b089378fefe96cfc397d
gee-dev29 <maduegodwin2605@gmail.com>
2025-06-17 10:54:38 +0100

    fix

--- app/Http/Requests/CreateCandidateRequest.php
+++ app/Http/Requests/CreateCandidateRequest.php
@@ -22,7 +22,92 @@ class CreateCandidateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'firstName' => [
+                'string',
+                'required',
+                'min:2',
+                'max:15',
+            ],
+            'lastName' => [
+                'required',
+                'min:2',
+                'max:15',
+                'string'
+            ],
+            'email' => [
+                'required',
+                'email',
+                'min:5',
+                'max:255',
+                'unique:users,email'
+            ],
+            'age' => [
+                'required',
+                'integer',
+                'min:2',
+                'max:2'
+            ],
+            'citizenship' => [
+                'required',
+                'string'
+            ],
+            'residency' => [
+                'required',
+                'min:2',
+                'string'
+            ],
+            'votingParty' => [
+                'required',
+                'string'
+            ],
+            'town' => [
+                'required',
+                'string'
+            ],
+            'lga' => [
+                'required',
+                'string'
+            ],
+            'stateOfOrigin' => [
+                'required',
+                'string'
+            ],
+            'birthPlace' => [
+                'required',
+                'string'
+            ],
+            'nationaliy' => [
+                'required',
+                'string'
+            ],
+            'roleId' => ['required', 'string'],
+            'roleId.*' => ['uuid'],
+            // 'password' => [
+            //     'required',
+            //     'max:255',
+            //     'string',
+            //     'confirmed',
+            //     \Illuminate\Validation\Rules\Password::min(8)
+            //         ->mixedCase()
+            //         ->numbers()
+            //         ->symbols()
+            // ],
+
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'firstName.string' => "first name must be a string",
+            'lastName.string' => "last name must be a string",
+            'email.email' => "email must be an email format",
+            'age.int' => "Age must be a integer",
+            'residency.string' => "Residency must be a string",
+            'town.string' => "Town must be a string",
+            'lga.string' => "LGA must be a string",
+            'stateOfOrigin.string' => "State of origin must be a string",
+            'town.string' => "Town must be a string",
         ];
     }
 }
