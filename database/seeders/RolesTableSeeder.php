f5154f6b74ec2ba490e6db91031b8c2803eb938c
gee-dev29 <maduegodwin2605@gmail.com>
2025-06-19 17:02:23 +0100

    repopulated my role data

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -2,18 +2,27 @@
 
 namespace Database\Seeders;
 
-use App\Models\Role;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RolesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        Role::create(['name' => 'admin']);
-        Role::create(['name' => 'voter']);
-        Role::create(['name' => 'organizer']);
+        $csvFile = fopen(base_path('database/data/roles.csv'), 'r');
+        $firstLine = true;
+        while (($data = fgetcsv($csvFile, 1000, ',')) !== false) {
+            if ($firstLine) {
+                $firstLine = false;
+                continue;
+            }
+            DB::table('role')->insert([
+                'id' => $data[0],
+                'roleName' => $data[1],
+                'created_at' => $data[2],
+                'updated_at' => $data[3],
+            ]);
+        }
+        fclose($csvFile);
     }
 }
