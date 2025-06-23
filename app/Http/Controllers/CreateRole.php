53d20028d86eeb707353e5d5473789164a60c913
gee-dev29 <maduegodwin2605@gmail.com>
2025-06-23 16:57:28 +0100

    fix

--- app/Http/Controllers/CreateRole.php
+++ app/Http/Controllers/CreateRole.php
@@ -2,19 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\CreateRoleRequest;
 use App\Http\ResponseInterface\ApiResponse;
 use App\Http\ResponseInterface\StatusCode;
 use App\Models\Role;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class CreateRole extends Controller
 {
-    public function __invoke(CreateUserRequest $request)
+    public function __invoke(CreateRoleRequest $request)
     {
         try {
-            $post = $request->getParsedBody();
+            $post = $request->all();
             $role = Role::createRole($post);
             $role->save();
             Log::info('User created successfully', ['userId' => $role->id()]);
