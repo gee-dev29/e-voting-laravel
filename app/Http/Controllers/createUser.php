50738a4fc7fd7b7d2c2a430c885e30b8bf328c24
gee-dev29 <maduegodwin2605@gmail.com>
2025-06-27 14:50:26 +0100

    fix

--- app/Http/Controllers/createUser.php
+++ app/Http/Controllers/createUser.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CreateUserRequest;
 use App\Http\ResponseInterface\ApiResponse;
 use App\Http\ResponseInterface\StatusCode;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 class CreateUser extends Controller
 {
@@ -17,10 +19,19 @@ class CreateUser extends Controller
             /** @var User */
             $user = User::createUser($post);
             $user->save();
+
+            $existingRoleIds = Role::whereIn('id', $post['roleId'])->pluck('id')->toArray();
+            if (count($existingRoleIds) !== count($post['roleId'])) {
+                $invalidRoleIds = array_diff($post['roleId'], $existingRoleIds);
+                throw ValidationException::withMessages([
+                    'roleId' => 'One or more provided role IDs are invalid: ' . implode(', ', $invalidRoleIds),
+                ]);
+            }
+            $user->roles()->attach($existingRoleIds);
             Log::info('User created successfully', ['userId' => $user->userId()]);
             return ApiResponse::success('User created successfully', StatusCode::CREATED);
         } catch (\Throwable $th) {
-            Log::error('Error creating user', ['error' => $th->getMessage()]);
+            Log::error('Error creating user', ['error' => $th->getMessage(), 'trace' => $th->getTraceAsString()]);
             return ApiResponse::error('Error creating user', $th->getMessage(), StatusCode::BAD_REQUEST);
         }
     }
