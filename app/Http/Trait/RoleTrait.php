eaec8aa2a124360dfe3a1b990c7c1762eb111461
gee-dev29 <maduegodwin2605@gmail.com>
2025-07-01 12:15:58 +0100

    fixed

--- app/Http/Trait/RoleTrait.php
+++ app/Http/Trait/RoleTrait.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Trait;
 
+use App\Http\Id\UserId;
 use App\Models\Role;
+use Illuminate\Support\Facades\DB;
 
 trait RoleTrait
 {
@@ -13,5 +15,21 @@ trait RoleTrait
     /** @var Role */
     $role = Role::find($roleId);
     return $role?->roleName();
+  }
+
+  private function getUserRole(UserId $userId): array|string
+  {
+    $roleData = [];
+    $userRoles = DB::table('role_user')->where(['userId' => $userId->toString()])->get();
+    foreach ($userRoles as $userRole) {
+      $role = Role::find($userRole->roleId);
+      if ($role) {
+        $roleData[] = [
+          'roleId' => $role->id,
+          'roleName' => $this->getRoleName($role->id)
+        ];
+      }
+    }
+    return $roleData;
   }
 }
