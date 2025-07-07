fc2252169cf2584d9f83d574dec65f8924a1a2ba
gee-dev29 <maduegodwin2605@gmail.com>
2025-07-07 13:24:34 +0100

    added to migration

--- database/migrations/2025_07_03_182130_recreate_role_permission_table.php
+++ database/migrations/2025_07_03_182130_recreate_role_permission_table.php
@@ -11,7 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        
+        Schema::dropIfExists('role_permission');
+
+        Schema::create('role_permission', function (Blueprint $table) {
+            $table->uuid('roleId');
+            $table->foreign('roleId')->references('id')->on('role')->onDelete('cascade');
+            $table->uuid('permissionId');
+            $table->foreign('permissionId')->references('id')->on('permission')->onDelete('cascade');
+
+            // Set a composite primary key to ensure uniqueness for each role-permission pair
+            $table->primary(['roleId', 'permissionId']);
+        });
     }
 
     /**
@@ -19,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('role_permission');
     }
 };
