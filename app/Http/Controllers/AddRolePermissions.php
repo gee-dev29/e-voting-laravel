022e65f45a1483c985d779f686ee5e452cf300b7
gee-dev29 <maduegodwin2605@gmail.com>
2025-07-08 11:12:04 +0100

    fixed

--- app/Http/Controllers/AddRolePermissions.php
+++ app/Http/Controllers/AddRolePermissions.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Id\PermissionId;
 use App\Http\ResponseInterface\ApiResponse;
 use App\Http\ResponseInterface\StatusCode;
-use Exception;
+use App\Models\Permission;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Validation\ValidationException;
 
 class AddRolePermissions extends Controller
 {
@@ -17,36 +14,32 @@ class AddRolePermissions extends Controller
     {
         // try {
         $role = $request->attributes->get('role');
-        $validatedData = $request->validated([
+        $validatedData = $request->validate([
             'permissionIds' => 'required|array',
             'permissionIds.*' => 'required|string|uuid'
         ]);
         $incomingPermissionIds = $validatedData['permissionIds'];
         $addedPermissionIds = [];
         foreach ($incomingPermissionIds as $permissionIdString) {
-            $permissionId = PermissionId::fromString($permissionIdString);
-            $addedPermissionIds[] = $permissionId;
-            // $permission = Permission::where(['id' => $permissionId->toString()])->first();
-            // if (!$permission) {
-            //     throw new Exception("Permission not found", 1);
-            // }
-            // $rolePermission = RolesPermissions::AddPermissionsToRole([
-            //     'roleId' => $roleId,
-            //     'permissionIds' => $permissionId->toString()
-            // ]);
-            $existingPermissionIds = DB::table('role_permission')->whereIn('permissionId', $permissionId->toString())->pluck('permissionId')->toArray();
-            if (count($existingPermissionIds) !== count($addedPermissionIds)) {
-                $invalidRoleIds = array_diff($incomingPermissionIds, $existingPermissionIds);
-                throw ValidationException::withMessages([
-                    'roleId' => 'One or more provided role IDs are invalid: ' . implode(', ', $invalidRoleIds),
-                ]);
+            // $permissionId = PermissionId::fromString($permissionIdString);
+            $permission = Permission::where(['id' => $permissionIdString])->exists();
+            if (!$permission) {
+                return ApiResponse::error("one or more provided permission IDs are invalid: `{$permissionIdString}` not found.", StatusCode::BAD_REQUEST);
             }
-            $role->permissions()->attach($existingPermissionIds);
+            $addedPermissionIds[] = $permissionIdString;
+            // get existing permission IDs to avoid duplicates
+            $existingPermissionIdString = $role->permissions()->pluck('id')->toArray();
+            $permissionsToAttach = array_diff($addedPermissionIds, $existingPermissionIdString);
+            if (empty($permissionsToAttach)) {
+                return ApiResponse::success('No new permission to add, or all specified permissions are already linked to this role.', statusCode::OK);
+            }
+            // $existingPermissionIds = DB::table('role_permission')->whereIn('permissionId', $permissionIdString)->pluck('permissionId')->toArray();
+            $role->permissions()->attach($addedPermissionIds);
             Log::info('Permission added to role successfully');
-            return ApiResponse::success('Permission(s) added to role successfully', StatusCode::CREATED);
+            return ApiResponse::success('Permission(s) successfully attached to role', StatusCode::CREATED);
         }
         // } catch (\Throwable $th) {
-        //     return ApiResponse::error('Error creating user', StatusCode::BAD_REQUEST);
+        //     return ApiResponse::error('An Internal server error occured while processing permissions', StatusCode::INTERNAL_SERVER_ERROR);
         // }
     }
 }
